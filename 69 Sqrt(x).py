59325f263c3f0a55399cffbd58b8c77f2e565662
reejung kim <reejung.kim@gmail.com>
2021-01-02 00:18:57 +0000

    Add solution

---
+++ 69 Sqrt(x).py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan  2 00:17:29 2021
+
+@author: reejungkim
+"""
+
+class Solution:
+    def mySqrt(self, x: int) -> int:
+        import math
+        return math.floor(sqrt(x))
