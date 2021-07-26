3f289ea1eeb6a5a23e0f66a14f45752450c5090b
reejung.kim <reejung.kim@gmail.com>
2021-07-26 20:51:54 +0900

    Add initial commit

---
+++ 12 Interger to Roman.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Jul 26 20:50:36 2021
+
+@author: reejungkim
+"""
+
+class Solution:
+    def intToRoman(self, num: int) -> str:
+        values = [ 1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1 ]
+        numerals = [ "M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I" ]
+        res = ""
+        for i, v in enumerate(values):
+            res += (num//v) * numerals[i]
+            num %= v
+        return res
